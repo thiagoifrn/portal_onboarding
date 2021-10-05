7c9abe95382146b2d6d6a3f76570b461a6b33221
Giulia de Carvalho <giulia.faggion@zup.com.br>
2021-10-05 17:06:17 -0300

    create strings.dart

---
+++ lib/app/utils/strings.dart
@@ -0,0 +1,53 @@
+class Strings {
+  // #region home page
+
+  static const String titleTextHomeApresentation = 'iupp! a gente chegou';
+  static const String subTitleTextHomeApresentation =
+      'Um canal que reúne as principais informações sobre o nosso produto para que você possa se inteirar e ficar por dentro de tudo o que rola por aqui';
+  static const String contextHomeApresentation =
+      'Esse portal foi totalmente desenvolvido pensando no bem estar dos nossos funcionários. Aqui você vai encontrar links para solicitar os primeiros acessos, materiais de estudos, um panorama das tecnologias que utilizamos e muito mais! ;)\n'
+      'O iupp se posiciona como um marketplace, onde 70% dos pagamentos são feitos via pontos, já possui em seu catálogo mais de quinhentos mil skus, fortes parcerias já estabelecidas, negociações comerciais interessantes e temos clientes com mais de 75 bilhões de pontos (~1,5 bi reais) e isto pode ser usado como ignição. Se quiser saber mais sobre o iupp e sobre o seu modelo de negócio, clique aqui, e acesse o nosso 6 pager. ';
+
+  // #endregion
+
+// #region first steps and access
+
+  static const String titleFirstStepsAndAccess = 'primeiros passos e acessos';
+  static const String titleJiraTopic = 'jira';
+  static const String titleGitbookTopic = 'gitbook';
+  static const String titleGithubTopic = 'github';
+  static const String titleVPNTopic = 'vpn';
+  static const String titleAWSTopic = 'aws';
+  static const String titleShortcutTopic = 'shortcut';
+
+// #endregion
+
+  // #region used techs
+
+  static const String titleBackTopic = 'back';
+  static const String titleFrontTopic = 'front';
+
+// #endregion
+
+  // #region used techs
+
+  static const String titleFlutterTopic = 'flutter';
+  static const String titleDockerTopic = 'docker (plataforma)';
+
+// #endregion
+
+// #region generic
+
+  static const String cardHomeAccess = 'acessos';
+  static const String cardUsedTechs = 'tecnologias utilizadas';
+  static const String cardStudyMaterials = 'materiais de estudo';
+  static const String cardGlossary = 'glossário';
+  static const String titleCleanArchTopic = 'arquitetura limpa';
+  static const String titleSRETopic = 'sre';
+// #endregion
+
+//Texto generico para colocar nos conteudos (porque ainda não tem todos
+static const String genericLongText = 'Lorem ipsum elit lacinia malesuada cras purus posuere ad libero, tellus non eget ultricies faucibus eros nulla nisi lobortis cras, faucibus cubilia aliquet cursus nisi risus eleifend egestas. nullam elementum nec lorem cursus condimentum inceptos vitae accumsan, fringilla semper velit mauris venenatis sollicitudin gravida nibh ad, vitae class non egestas mattis duis per. Habitant fames fusce viverra massa himenaeos ligula ultricies aliquam suspendisse condimentum, sollicitudin quisque urna mollis platea mollis donec etiam ut, curabitur eu class justo euismod ipsum semper quam tortor. imperdiet aliquet phasellus potenti velit egestas iaculis nibh, maecenas pellentesque odio adipiscing aliquam tincidunt varius, vel suspendisse dui euismod urna ultrices. primis urna aptent primis consequat imperdiet magna egestas eget ligula cras lobortis mattis, himenaeos nam in justo suscipit mattis malesuada dictumst venenatis ullamcorper fringilla morbi magna, potenti diam urna morbi torquent euismod aliquet condimentum vivamus aenean posuere. varius eget facilisis felis suspendisse faucibus integer conubia, purus suscipit nisi sagittis dictumst quisque, morbi vel erat sagittis lectus sociosqu. Risus potenti justo quisque faucibus himenaeos vitae amet erat morbi, aenean vulputate tortor vel aptent facilisis primis eget quisque curabitur, dictum vitae duis sit felis congue neque bibendum. condimentum praesent suspendisse tempor fringilla tellus mattis accumsan quisque purus turpis faucibus, molestie pretium maecenas netus eget praesent morbi mauris neque primis. hendrerit facilisis porttitor habitant vestibulum pulvinar maecenas nisl dolor pharetra fringilla, felis nisl tristique et dictum maecenas ut conubia iaculis molestie auctor, massa class lectus sed purus blandit proin faucibus fusce. tincidunt nisl himenaeos risus nulla ligula donec aliquet ultrices, amet integer a ante tortor fusce erat enim, adipiscing integer accumsan aliquet semper nullam quisque. Dui fames condimentum quis dapibus suspendisse praesent sapien eleifend mi nulla interdum, quam odio purus potenti posuere eleifend ut at pulvinar tellus, lobortis sociosqu faucibus placerat blandit convallis lobortis aliquet conubia libero. aptent tempus interdum aenean cras curabitur erat dui nulla faucibus egestas donec enim, senectus aliquam id auctor nostra curae dapibus at posuere morbi. congue per diam in vitae curabitur feugiat nec nulla felis porta, non netus quisque suscipit dictumst cursus sit blandit nullam. elit arcu suscipit tristique pulvinar mattis phasellus, quisque condimentum magna torquent consectetur per quam, velit pellentesque molestie tempus ipsum. ';
+  static const String genericSmallText = 'Lorem ipsum elit lacinia malesuada cras purus posuere ad libero, tellus non eget ultricies faucibus eros nulla nisi lobortis cras, faucibus cubilia aliquet cursus nisi risus eleifend egestas. nullam elementum nec lorem cursus condimentum inceptos vitae accumsan, fringilla semper velit mauris venenatis sollicitudin gravida nibh ad, vitae class non egestas mattis duis per. Habitant fames fusce viverra massa himenaeos ligula ultricies aliquam suspendisse condimentum, sollicitudin quisque urna mollis';
+
+}
